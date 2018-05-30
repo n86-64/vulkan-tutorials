eecd60ba5befdb74100aea40417582b0d7cbf5e6
Nathan <nathan.butt@gmail.com>
2018-05-30 17:38:43 +0100

    Added new project and libaries
    MAJOR CHANGES:
    * Added GLM library support to all existing projects.
    
    * Added new project to create a basic vulkan application.

---
+++ vkTutorials/tutorial2/GameClass.h
@@ -0,0 +1,16 @@
+/*
+*  Vulkan tutorials - Tutorial 2
+*  Subject - A basic example drawing a triangle.
+*  Date - 29/05/18
+*/
+
+#pragma once
+
+
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+
+class VKGame 
+{
+
+};
---
+++ vkTutorials/tutorial2/vkMath.h
@@ -0,0 +1,14 @@
+/*
+*   OpenGL/Vulkan math helper header
+*   Discription - Wraps all of the math functions into a convineant function wrapper.
+*   Date - 30/05/18
+*/
+
+#pragma once
+
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/common.hpp>
+
+// TODO - Include other GLM entries here.
